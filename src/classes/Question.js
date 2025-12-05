e73fe1db2a0498c1281e2a73f99d8febac7cb46d
elifyilmaz00 <elifyilmaz1906@gmail.com>
2025-12-05 17:35:04 +0300

    feat(oop): Implement question class with constructor and validation method

--- src/classes/Question.js
+++ src/classes/Question.js
@@ -1,1 +1,27 @@
 //Soru Sınıfı
+//Bu sınıfta başka dosyalarda kullanılabilmesi için export edilir
+
+export class Question{
+    /**
+     * Yeni bir soru nesnesi oluşturur
+     * @param {string} text - soru metni
+     * @param {strimg[]} options - cevap seçeneklerinden oluşan bir dizi
+     * @param {string} answer - doğru cevap metni
+     */
+
+    constructor(text, options, answer){
+        this.text=text;
+        this.options=options;
+        this.answer=answer;
+    }
+
+    /**
+     * verilen bir seçeneğin doğru cevap olup olmadığını kontrol eder
+     * @param {string} choice - kullanıcının seçtiği cevap
+     * @returns {boolean} - eğer cevap doğruysa true, yanlışsa false döndürür
+     */
+
+    isCorrectAnswer(choice){
+        return this.answer=== choice;
+    }
+}
