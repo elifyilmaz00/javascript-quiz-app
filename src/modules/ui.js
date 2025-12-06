7084efce26e3ff6904a803052e29ba7c572c8db1
elifyilmaz00 <elifyilmaz1906@gmail.com>
2025-12-06 17:05:06 +0300

    feat(oop/ui): Implement UI class for quiz interface management

--- src/modules/ui.js
+++ src/modules/ui.js
@@ -1,1 +1,51 @@
 //DOM/Arayüz İşlemleri
+
+export class UI{
+    consturctor(){
+        this.questionEl=document.querySelector("#question");
+        this.optionsListEl=document.querySelector("#options-list");
+        this.progressEl=document.querySelector("#progress");
+        this.quizContainerEl=document.querySelector("#quiz");
+    }
+
+    /**
+     * ekrana soruyu ve seçenekleri yazar
+     * @param {Question} question - gösterilecek olan Question nesnesi
+     */
+
+    displayQuestion(question){
+        this.questionEl.textContent=question.text;
+        this.optionsListEl.innerHTML=""; //önceki seçenekleri temizler
+
+        question.options.forEach(option => {
+            const li = document.createElement("li");
+            li.textContent=option;
+            this.optionsListEl.appendChild(li);
+        });
+    }
+
+    /**
+     * quiz bittiğinde sonuç ekranını gösterir
+     * @param {number} score kullanıcının toplam doğru sayısı
+     * @param {number} totalQuestions - toplam soru sayısı
+     */
+
+    displatScore(score, totalQuestions){
+        const scoreHTML = `
+        <h1> Quiz Bitti!</h1>
+        <h2> Skorunuz: ${score} / ${totalQuestions}</h2>
+        <button onclick ="location.reload()" style="padding: 10px 20px; curdor: pointer;">Tekrar Oyna</button>`;
+        this.quizContainerEl.innerHTML=scoreHTML;
+
+    }
+
+    /**
+     * ilerleme metnini günceller (örn: soru 2/5)
+     * @param {number} currentIndex - mevcut sorunun indeksi
+     * @param {number} totalQuestions - toplam soru sayısı
+     */
+
+    displayProgress(currentIndex, totalQuestions){
+        this.progressEl.textContent=`Soru ${currentIndex +1} / ${totalQuestions}`;
+    }
+}
